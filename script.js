239dcd719715ba8eb949e560ea31fbccd3ba96da
bespokesamples <bespokesamples@gmail.com>
2025-05-26 21:02:11 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function change() {
+    if (document.getElementById("home-title").style.color == "white") {
+        document.getElementById("home-title").style.color = "red";
+    } else {
+        document.getElementById("home-title").style.color = "white";
+    }
+}
+
+function caruselRight {
+    
+}
+function caruselLeft {
+    
+}
